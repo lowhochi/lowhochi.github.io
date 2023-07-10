9ffba1b878edc4c2a23a538c60bdbec8542435df
Tony Low <lowhochi2012@gmail.com>
2023-07-10 20:17:14 +0800

    Update myVocabPicture.js

--- myVocabPicture.js
+++ myVocabPicture.js
@@ -1,12 +1,13 @@
 var checks = { zebra_crossing: false };
 
-function showPicture(nameID, check){
-  alert("HELLO WORLD");
-  if (check==true){
-    document.getElementById(nameID).style.display = "none";
-    check = false;
-  }else{
-    document.getElementById(nameID).style.display = "block";
-    check = true;
-  }
+function showPicture(input, checks){
+        alert("HELLO WORLD");
+        nameID = "pic_"+input;        
+        if (check==true){
+                document.getElementById(nameID).style.display = "none";
+                checks[input] = false;
+        }else{
+                document.getElementById(nameID).style.display = "block";
+                checks[input] = true;
+        }
 }
