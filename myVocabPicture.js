057a433fd04e5f18cf0aa6cf89e40a9aedf2c9d0
Tony Low <lowhochi2012@gmail.com>
2023-07-10 20:18:22 +0800

    Update myVocabPicture.js

--- myVocabPicture.js
+++ myVocabPicture.js
@@ -1,7 +1,7 @@
 var checks = { zebra_crossing: false };
 
 function showPicture(input, checks){
-        alert("HELLO WORLD");
+        alert("checks[input] = "+checks[input]);
         nameID = "pic_"+input;        
         if (check==true){
                 document.getElementById(nameID).style.display = "none";
