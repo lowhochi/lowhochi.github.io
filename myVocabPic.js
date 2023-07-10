f88c9c95aaddf9b3fb43137b0e35362243ce1408
Tony Low <lowhochi2012@gmail.com>
2023-07-10 17:39:32 +0800

    Update myVocabPic.js

--- myVocabPic.js
+++ myVocabPic.js
@@ -1,2 +1,10 @@
 function showPicture(nameID, check){
+  alert("hello world");
+  if (check==true){
+    document.getElementById(nameID).style.display = "none";
+    check = false;
+  }else{
+    document.getElementById(nameID).style.display = "block";
+    check = true;
+  }
 }
