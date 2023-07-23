a7dc3fb44332a5d0cfbdb5880d300342f87a750c
Tony Low <lowhochi2012@gmail.com>
2023-07-23 15:50:22 +0800

    Update myVocabGo.js

--- myVocabGo.js
+++ myVocabGo.js
@@ -1,7 +1,26 @@
-//s = 01, 02, 03, ... and so on
+//input = 00, 01, 02, 03, ... and so on
+const mySet = new Set();
+mySet.add("00");
+mySet.add("01");
+mySet.add("02");
+mySet.add("03");
+mySet.add("04");
+mySet.add("05");
+mySet.add("06");
+mySet.add("07");
+mySet.add("08");
+mySet.add("09");
+mySet.add("10");
+mySet.add("11");
+mySet.add("12");
+mySet.add("13");
+
 function goToPage(s){
         var myLink = "https://lowhochi.github.io/myVocab";
 	var temp = document.getElementById(s).value;
+	if (!mySet.has(temp)){
+		return;
+	}
 	if (temp=="00"){
 		temp = "Search";
 	}
