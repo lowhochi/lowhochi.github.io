990d6b473aa9519dd7a630be552bb7e4aa289102
Tony Low <lowhochi2012@gmail.com>
2025-03-14 13:27:33 +0800

    Update myVocabGo.js

--- myVocabGo.js
+++ myVocabGo.js
@@ -1,9 +1,9 @@
 //input = 00, 01, 02, 03, ... and so on
 const mySet = new Set();
 
-var p = 23;
+var page = 23;
 var ss = "";
-for (let i=0; i<p+1; i++){
+for (let i=0; i<page+1; i++){
 	if (i<10){
 		ss = "0" + i.toString();
 	}else{
